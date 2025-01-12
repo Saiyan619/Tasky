b0729f5ba6c4c5e3b4600ef27a5d378d856f82d6
olaniyi <arokoyueb11@gmail.com>
2025-01-12 12:26:33 +0100

    fixing user profile saving function speed issue

--- app/page.js
+++ app/page.js
@@ -1,95 +1,82 @@
-"use client"
+"use client";
+
 import { useUser } from "@clerk/nextjs";
 import { useState, useEffect } from "react";
 import GlobalApi from "@/app/_utils/GlobalApi";
 import Link from "next/link";
+import { useRouter } from "next/navigation"; 
 
 export default function Home() {
+  const router = useRouter();
   const { user } = useUser();
-  // const [userList, setUserList] = useState([])
-const [allowClick, setAllowClick] = useState(false)
+  const [allowClick, setAllowClick] = useState(false);
 
+  const navigateToHomePage = () => {
+    router.push("/home"); 
+  };
 
   useEffect(() => {
-    // getAllUsers();
-   user && createUsers()
-    console.log('this is home')
-    
-  }, [user])
+    if (user) {
+      createUsers();
+      console.log("This is home");
+    }
+  }, [user]);
 
   const createUsers = async () => {
-    setAllowClick(true);
-    try {
-        const data = {
-            clerkId: user?.id,
-            name: user?.fullName,
-            email: user?.primaryEmailAddress?.emailAddress,
-            createdAt: user?.createdAt
-        };
-        
-        const resp = await GlobalApi.createUser(data);
-        console.log(resp);
-        console.log('user created');
-        setAllowClick(false);
-    } catch (error) {
-        console.log(error);
-    } 
-};
-  
-  
-  // const getAllUsers = async () => {
-  //   try {
-  //     GlobalApi.getUsers().then(resp => {
-  //       setUserList(resp.data)
-  //       console.log(resp.data)
-  //     })
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+    if (user) {
+      navigateToHomePage();
+    }
 
+    setAllowClick(true);
+
+    try {
+      const data = {
+        clerkId: user?.id,
+        name: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        createdAt: user?.createdAt,
+      };
+
+      const resp = await GlobalApi.createUser(data);
+      console.log(resp);
+      console.log("User created");
+    } catch (error) {
+      console.error("Error creating user:", error);
+    } finally {
+      setAllowClick(false); 
+    }
+  };
 
   return (
     <div className="hero bg-base-200 min-h-screen">
-    <div className="hero-content text-center">
-      <div className="max-w-md">
-        <h1 className="text-5xl font-bold">Hello 👋</h1>
-        <span className="text-2xl font-bold">Welcome to Tasky</span>
-        <p className="py-6">
-         Make collaboration and organization of Task easier
-        </p>
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello 👋</h1>
+          <span className="text-2xl font-bold">Welcome to Tasky</span>
+          <p className="py-6">
+            Make collaboration and organization of tasks easier
+          </p>
 
-          
-
-
-          {!allowClick ? 
-              ( <button className="btn btn-primary">
-                <Link href="/home">
-                Get Started📝
-                    </Link>
-                </button>
-            )
-              :
-            (   <button  disabled="disabled" className="btn">
+          {!allowClick ? (
+            <button className="btn btn-primary">
+              <Link href="/home">Get Started📝</Link>
+            </button>
+          ) : (
+            <button disabled className="btn">
               <span className="loading loading-spinner"></span>
-              <Link href="/home">
-              please wait...
-                          </Link>
-            </button>)
-            
-          
-      }    
+              <Link href="/home">Please wait...</Link>
+            </button>
+          )}
 
-          {!allowClick ? 
-            (<div></div>)
-            :
-          (  <div>
-            <span className="text-sm font-bold text-gray-600">Please wait, saving user profile, might take a minute....</span>
-          </div>)
-}
-  
+          {allowClick && (
+            <div>
+              <span className="text-sm font-bold text-gray-600">
+                Please wait, saving user profile, might take a minute...
+              </span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  </div>
   );
 }
